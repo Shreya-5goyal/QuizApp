aa19dc2b0d104e9dca6e10fbd2202d565d0a9cc1
shreya goel <131802153+Shreya-5goyal@users.noreply.github.com>
2024-10-24 19:05:58 +0530

    Add files via upload

---
+++ eeee/src/test/java/sample/com/eeee/EeeeApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.com.eeee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
