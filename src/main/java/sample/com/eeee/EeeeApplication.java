2d0dc291db92b33353ca405183e8d5b691765aaf
shreya goel <131802153+Shreya-5goyal@users.noreply.github.com>
2024-10-24 19:09:15 +0530

    Add files via upload

---
+++ src/main/java/sample/com/eeee/EeeeApplication.java
@@ -0,0 +1,15 @@
+package sample.com.eeee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeeeApplication.class, args);
+
+
+	}
+
+}
